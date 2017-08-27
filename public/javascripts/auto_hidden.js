63fb963645842efe9dc8d186ccfe056c3e9d0436
julien-elodie <2644756656@qq.com>
2017-08-27 17:56:39 +0800

    init project

---
+++ public/javascripts/auto_hidden.js
@@ -0,0 +1,12 @@
+window.onresize(function () {
+  // 获取窗口宽度
+  if (window.innerWidth) {
+    winWidth = window.innerWidth;
+  }
+  else if ((document.body) && (document.body.clientWidth)) {
+    winWidth = document.body.clientWidth;
+  }
+  // 动态隐藏侧边栏
+  var main_menu = document.getElementById('main_menu');
+  main_menu.setAttribute('display', 'none');
+});

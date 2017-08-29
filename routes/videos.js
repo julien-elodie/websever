0f1ef700f83e0f4afaa59ae5eca3b2cf462f31b5
julien-elodie <2644756656@qq.com>
2017-08-29 19:16:05 +0800

    init project

--- routes/videos.js
+++ routes/videos.js
@@ -1,8 +1,29 @@
 var express = require('express');
+var pg = require('pg');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
-  res.render('YouTube');
+var config = {
+  user:'julien-elodie',
+  database:'videoinfodb',
+  password:'wyq2644756656',
+  port:'5432'
+};
+
+var client = new pg.Client(config);
+
+client.connect(function (err) {
+  if (err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('select * from videoadvanceinfo;',function (err, result) {
+    if (err) {
+      return console.error('error running query', err);
+    }
+    // return result.rows[0];
+    router.get('/', function (req, res, next) {
+      res.render('videos', result.rows[0]);
+    });
+  })
 });
 
 module.exports = router;

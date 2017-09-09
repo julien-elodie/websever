33fe88617453e2482c72f8f9f1a5a2ccfe8c6ba8
julien-elodie <2644756656@qq.com>
2017-09-09 16:51:14 +0800

    init project

--- public/javascripts/login_focus.js
+++ public/javascripts/login_focus.js
@@ -1,35 +1,74 @@
-function login_username_focus() {
-    var login_username_text = $('.login_username_text');
-    var username = $('.username');
+$(window).on("beforeunload", function() {
+    var username = $(".username");
+    username.val("");
+});
 
-    login_username_text.css('display', 'inline');
-    username.attr('placeholder', '');
+function login_username_focus() {
+    var login_username_text = $(".login_username_text");
+    var username = $(".username");
+
+    login_username_text.css("display", "inline");
+    username.attr("placeholder", "");
 }
 
 function login_password_focus() {
-    var login_password_text = $('.login_password_text');
-    var password = $('.password');
+    var login_password_text = $(".login_password_text");
+    var password = $(".password");
 
-    login_password_text.css('display', 'inline');
-    password.attr('placeholder', '');
+    login_password_text.css("display", "inline");
+    password.attr("placeholder", "");
 }
 
 function login_username_blur() {
-    var login_username_text = $('.login_username_text');
-    var username = $('.username');
+    var login_username_text = $(".login_username_text");
+    var username = $(".username");
+    var password = $(".password");
 
-    if (!username.val()) {
-        login_username_text.css('display', 'none');
-        username.attr('placeholder', '用户名');
+    var username_hint_container = $(".username_hint_container");
+    if (username.val() == "") {
+        username_hint_container.html('<div class="username_hint">请填写用户名!</div>');
+    } else if (!username.val()) {
+        login_username_text.css("display", "none");
+        username.attr("placeholder", "用户名");
+    } else {
+        username_input = username.val();
+        data = {
+            username: username_input
+        };
+        options = {
+            type: "post",
+            url: "../Youtube",
+            data: data,
+            dataType: "json",
+            success: function(res) {
+                username_hint_container.html("");
+                password.attr("alt", res.password);
+            },
+            error: function(res) {
+                username_hint_container.html(
+                    '<div class="username_hint" color="red">用户名错误!</div>'
+                );
+            }
+        };
+        $.ajax(options);
     }
 }
 
 function login_password_blur() {
-    var login_password_text = $('.login_password_text');
-    var password = $('.password');
+    var login_password_text = $(".login_password_text");
+    var password = $(".password");
 
-    if (!password.val()) {
-        login_password_text.css('display', 'none');
-        password.attr('placeholder', '密码');
+    var password_hint_container = $(".password_hint_container");
+    if (password.val() == "") {
+        password_hint_container.html('<div class="password_hint">请填写密码!</div>');
+    } else if (!password.val()) {
+        login_password_text.css("display", "none");
+        password.attr("placeholder", "密码");
+    } else if (password.val() == password.attr("alt")) {
+        password_hint_container.html("");
+    } else {
+        password_hint_container.html(
+            '<div class="username_hint" color="red">密码错误!</div>'
+        );
     }
 }
--- routes/YouTube.js
+++ routes/YouTube.js
@@ -1,55 +1,84 @@
-var express = require('express');
-var pg = require('pg');
+var express = require("express");
+var pg = require("pg");
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
     /*congfig*/
     var config = {
-        user: 'julien-elodie',
-        database: 'videoinfodb',
-        password: 'wyq2644756656',
-        port: '5432'
+        user: "julien-elodie",
+        database: "videoinfodb",
+        password: "wyq2644756656",
+        port: "5432"
     };
 
     var client = new pg.Client(config);
 
     client.connect(function(err) {
         if (err) {
-            return console.error('could not connect to postgres', err);
+            return console.error("could not connect to postgres", err);
         }
-        client.query('select * from videobaseinfo;', function(err, result) {
+        client.query("select * from videobaseinfo;", function(err, result) {
             if (err) {
-                return console.error('error running query', err);
+                return console.error("error running query", err);
             }
             res.locals.datasets = result.rows.slice(0, 5);
-            res.render('YouTube');
-        })
+            res.render("YouTube");
+        });
     });
 });
 
-router.post('/', function(req, res, next) {
+router.post("/", function(req, res, next) {
     var user_info = req.body;
-    res.send(user_info);
-})
+    if (user_info.password) {
+        res.send('OK!');
+    } else if (user_info.username) {
+        var username = user_info.username;
+        var config = {
+            user: "julien-elodie",
+            database: "videoinfodb",
+            password: "wyq2644756656",
+            port: "5432"
+        };
 
-router.get('/li', function(req, res, next) {
+        var client = new pg.Client(config);
+
+        client.connect(function(err) {
+            if (err) {
+                return console.error("could not connect to postgres", err);
+            }
+
+            client.query(
+                "select * from userinfo where username = '" + String(username) + "';",
+                function(err, result) {
+                    if (err) {
+                        return console.error("error running query", err);
+                    } else {
+                        res.send(result.rows[0]);
+                    }
+                }
+            );
+        });
+    }
+});
+
+router.get("/li", function(req, res, next) {
     /*congfig*/
     var config = {
-        user: 'julien-elodie',
-        database: 'videoinfodb',
-        password: 'wyq2644756656',
-        port: '5432'
+        user: "julien-elodie",
+        database: "videoinfodb",
+        password: "wyq2644756656",
+        port: "5432"
     };
 
     var client = new pg.Client(config);
 
     client.connect(function(err) {
         if (err) {
-            return console.error('could not connect to postgres', err);
+            return console.error("could not connect to postgres", err);
         }
-        client.query('select * from videobaseinfo;', function(err, result) {
+        client.query("select * from videobaseinfo;", function(err, result) {
             if (err) {
-                return console.error('error running query', err);
+                return console.error("error running query", err);
             }
             if (req.query.page <= result.rows.length / 5) {
                 var page = req.query.page;
@@ -57,8 +86,8 @@ router.get('/li', function(req, res, next) {
             } else {
                 res.send(null);
             }
-        })
+        });
     });
-})
+});
 
 module.exports = router;
